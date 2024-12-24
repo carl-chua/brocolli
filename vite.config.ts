1cf1384e33880ed9434c1daa7a7c7987d56da120
carl-chua <carlchua0701@gmail.com>
2024-12-25 00:19:26 +0800

    feat: vite boilerplate

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/vite-project/', // Replace with your repository name
+  plugins: [react()],
+  server: {
+    host: true, // This will bind the server to your local IP address
+  },
+});
