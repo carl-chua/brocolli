e40437faba7852a23d5cec21243d844b97f2ef81
carl-chua <carlchua0701@gmail.com>
2024-12-27 22:48:53 +0800

    feat: change header to fixed

--- src/components/ui/navbar/index.tsx
+++ src/components/ui/navbar/index.tsx
@@ -1,6 +1,6 @@
 export const Navbar = () => {
   return (
-    <nav className="w-full bg-primary p-4 text-white shadow-xl">
+    <nav className="fixed top-0 w-full bg-primary p-4 text-white shadow-xl">
       <div className="flex items-center justify-between">
         <div className="text-lg font-bold">Brocolli & Co</div>
       </div>
--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -7,7 +7,7 @@ export const HomePage = () => {
   const showDialog = useAtomValue(showDialogAtom);
 
   return (
-    <div className="mx-auto flex w-full max-w-7xl flex-grow flex-col items-center justify-center gap-y-4">
+    <div className="mx-auto mt-16 flex w-full max-w-7xl flex-grow flex-col items-center justify-center gap-y-4">
       <HeroSection />
 
       {/* Conditionally render the dialog to reset after submission */}
